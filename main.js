00e875bc54deed2a68a905f95d586a51c342551d
rudberga <rudberg@pm.me>
2020-12-08 02:08:12 +0000

    testing

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 var xhr = new XMLHttpRequest();
+var data;
+
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+xhr.send();
+
+function setData(jsonData) {
+    data = jsonData;
+}
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         //console.log(typeof(JSON.parse(this.responseText)));// checking if its an object
         //document.getElementById("data").innerHTML = this.responseText;// picking up data
-        console.log(JSON.parse(this.responseText)); //object logged to console
+        //console.log(JSON.parse(this.responseText)); //object logged to console
+        //setData(json.parse(this.responseText))
+        data = JSON.parse(this.responseText);
     }
 };
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-
-xhr.send();
+setTimeout(function(){
+    console.log(data);
+}, 500);
